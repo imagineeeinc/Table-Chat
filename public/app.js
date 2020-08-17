55e35c619e71bcc6d5cefb64ed4c91eacb3ffb6f
Imagineee <imagineeelego@gmail.com>
2020-08-17 19:36:23 +0100

    Add files via upload

---
+++ public/app.js
@@ -0,0 +1,166 @@
+var cache = []
+window.onload = function() {
+    //set the height of the chat box
+    var p = document.getElementById("pbox").offsetHeight
+    var screen = window.innerHeight - 120 - p - 30
+    screen = screen + "px"
+    document.getElementById("box").style.height = screen
+    //set the widhth of the message box
+    var s = document.getElementById("send").offsetWidth
+    var width = window.innerWidth - s - 40
+    width = width + "px"
+    document.getElementById("in").style.width = width
+    //set names
+    showname()
+    save()
+    //sendname()
+    //other things
+    document.getElementById('in').focus();
+    uncache()
+};
+
+var socket = io();
+console.log('%cJoined Server', "background-color: lightsalmon;")
+console.log('%cJavaScript is Loading...', "background-color: lightyellow;")
+console.log('%cServer And Client is ready', "background-color: lightgreen;")
+/*socket.on('connection', function(){
+  console.log('made socket connection');
+});*/
+//const socket = io.connect('http://localhost:4000');
+
+// Query DOM elements
+const message = document.getElementById('in');
+const output = document.getElementById('chatbox');
+const people = document.getElementById('people');
+const uname = document.getElementById('name')
+
+//keypress
+document.onkeypress = function (e) {
+    e = e || window.event;
+    // use e.keyCode
+    //console.log(event);
+    if (event.keyCode === 13) {//"&& event.ctrlKeyuse" for using combinations like 'ctrl+_'
+    save()
+    sendit()
+};
+
+function sendit() {
+    console.log("hi")
+    if(message.value.length > 0){
+        /*socket.emit('chat', {
+            message: message.value,
+        });*/
+        socket.emit('chat', document.getElementById('in').value, uname.value);
+      }
+      //once the message is sent, reset the innerHTML of the message div to an empty string
+        message.value = "";
+    }
+}
+//create date object
+var date = new Date().toDateString();
+
+socket.on('chat message', function(msg, name){
+    var d = new Date
+    var time = d.getHours() + ":" + d.getMinutes() 
+    var out = document.createElement("li")
+    out.innerHTML =  msg + "<h6>" + time + "</h6>" + "<h6>" + name + "</h6>";
+    if (name == uname.value) {
+        out.style.float = "right"
+        out.style.background = "lightgreen"
+    }
+    output.append(out);   
+    out = document.createElement("br")
+    if (name == uname.value) {
+        out.style.float = "right"
+    }
+    output.append(out);
+    var m = msg
+    cacheit(m)
+  });
+  socket.on('typing', function(data){
+    output.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+socket.on('name set', function(name, oldname){
+    if (name != uname.value) {
+        var out = document.createElement("li")
+        out.innerHTML =  name
+        out.setAttribute("id", name)
+        people.append(out);
+        document.getElementById("oldname").remove();
+    }
+  });
+  socket.on('typing', function(data){
+    output.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+function check_web_storage_support() {
+    if(typeof(Storage) !== "undefined") {
+        return(true);
+    }
+    else {
+        alert("Web storage unsupported!");
+        return(false);
+    }
+}
+
+function showname() {
+    if(check_web_storage_support() == true) {
+        result = localStorage.getItem('name');
+        document.getElementById('you').innerHTML = result;
+    }
+    if(result === null) {
+        result = "";
+        document.getElementById('you').innerHTML = "you";
+    }
+    uname.value = result;
+}
+
+function save() {
+    if(check_web_storage_support() == true) {
+        var name = uname;
+        if(name.value != '') {
+            localStorage.setItem("name", name.value);
+            socket.emit('name', uname.value, localStorage.getItem('oldname'));
+        }
+        else {
+        }
+    }
+}
+function saveold() {
+    if(check_web_storage_support() == true) {
+        var name = uname;
+        if(name.value != '') {
+            localStorage.setItem("oldname", name.value);
+        }
+        else {
+        }
+    }
+}
+function sendname() {
+    socket.emit('name', uname.value, localStorage.getItem('oldname'));
+}
+function cacheit(msg) {
+    if (cache.length < 20){
+        cache.push(msg)
+    } else {
+        cache.shift()
+        cache.push(msg)
+    }
+    if(check_web_storage_support() == true) {
+        localStorage.setItem("cache", JSON.stringify(cache));//JSON.stringify(cache) JSON.parse(localStorage.getItem("cache"))
+    }
+}
+function uncache() {
+    cache = JSON.parse(localStorage.getItem("cache"))
+    if (cache != null ) {
+        var i = 0;
+        do {
+            var out = document.createElement("li")
+            out.innerHTML =  cache[i];
+            output.append(out);   
+            out = document.createElement("br")
+            output.append(out);
+            i++;
+        }
+        while (i < cache.length);
+    }
+}
---
+++ server.js
@@ -0,0 +1,46 @@
+// require express first
+var express = require("express");
+var io = require('socket.io');
+var app = express();
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//now create a server
+//When the server starts listening on port 4000 then fire a callbak function
+// The callback function will console.log a string 
+var server = app.listen(3000, function(){
+ console.log("Listening to requests on port 4000");
+});
+// serve a static file to the browser 
+app.use(express.static("public"));
+
+//declare var io which is a reference to a socket connection made on the server
+var io = io(server);
+var cache = [];
+//Then use the io.on method which looks for a connection
+//upon a connection execute a callback function which will console.log something
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    io.emit('new', cache);
+    socket.on('chat', (msg, name) => {
+        //console.log('message: ' + msg);
+        if (cache.length < 40){
+            cache.push(msg)
+        } else {
+            cache.shift()
+            cache.push(msg)
+        }
+        io.emit('chat message', msg, name);
+      });
+      socket.on('name', (name, oldname) => {
+        io.emit('name set', name, oldname);
+      });
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      io.emit('disconnetct', );
+    });
+  });
+//console.log(socket.connected);
+//console.log(socket.id);
+//console.log(socket.disconnected);
